972357dde2da17b32cbb2eac0c6615d8d6c2047f
unknown <allenmk20073@yahoo.com>
2021-01-24 12:41:27 -0500

    added Tabs to the page

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +15,18 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+
+  const tabTag = document.createElement('div');
+  tabTag.classList.add('topics');
+
+  topics.forEach(topic => {
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    tabTag.append(tab);
+  })
+
+  return tabTag;
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +37,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  axios
+    .get(`https://lambda-times-api.herokuapp.com/topics`)
+    .then(response => {
+      let target = document.querySelector(selector);
+      target.append(Tabs(response.data.topics));
+    })
+
 }
 
 export { Tabs, tabsAppender }
